5ad1a4c5f95bb50d5a64b4d6c20ca95cbf1a0fda
RyanC30 <Ryan.P.Crane@students.ittralee.ie>
2017-04-29 15:06:24 +0100

    Added NegAddition method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,6 +12,10 @@ public class Calculator {
             return a + b;
         }
 
+    public double addNeg ( double a, double b){
+        return a + b;
+    }
+
     public double subtract(double a, double b) {
         return a - b;
     }
